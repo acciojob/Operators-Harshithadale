ac8e8757cb35baa6e472514bb1ebe7574e5f7359
Harshithadale <daleharshi6045@gmail.com>
2025-06-13 10:42:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your JS code here. If required.
+const num1  = document.getElementById("num1")
+const num2  = document.getElementById("num2")
+const calculate  = document.getElementById("calculate")
+const selectOperator = document.getElementById("select-operator");
+const result = document.getElementById("result");
+let operator = "+"
+selectOperator.addEventListener("change",(e) =>{
+	operator = e.target.value;
+	
+})
+calculate.addEventListener( "click", ()=>{
+	switch(operator){
+	case "+":
+		result.textContent = Number(num1.value)+Number(num2.value);
+			break
+	case "-":
+		result.textContent =Number(num1.value)-Number(num2.value);
+			break
+	case "*":
+		result.textContent =Number(num1.value)*Number(num2.value);
+			break
+	case "/":
+		result.textContent =Number(num1.value)/Number(num2.value);
+			break
+}
+})
+
+
